f6c63aa45676512e036f5d7225e45764ca3e4fe8
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-28 15:07:06 +0100

    finished up styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import '../App.css';
-import { NavLink } from 'react-router-dom';
+/* eslint-disable */
+import React from "react";
+import "../App.css";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => (
-  <div className="navbar">
-    <div className="logo">
-      <span className="head-logo">
-        <h1>Bookstore CMS</h1>
-      </span>
-      <ul>
-        <NavLink to="/">
-          <li>BOOKS</li>
-        </NavLink>
-        <NavLink to="/category">
-          <li>CATEGORIES</li>
-        </NavLink>
-      </ul>
+    <div className="navbar">
+        <div className="logo">
+            <span className="head-logo">
+                <h1>Bookstore CMS</h1>
+            </span>
+            <ul>
+                <NavLink to="/book-store" className="link-style">
+                    BOOKS
+                </NavLink>
+                <NavLink to="/category" className="link-style">
+                    CATEGORIES
+                </NavLink>
+            </ul>
+            <div className="user-circle">
+                <FontAwesomeIcon icon="user" className="custom-icon" />
+            </div>
+        </div>
     </div>
-  </div>
 );
 
 export default Navbar;
+/* eslint-enable */
